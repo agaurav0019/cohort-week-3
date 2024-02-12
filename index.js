93414e9f36d4122e9494dc5b67ddd184d480f15f
Abhishek Gaurav <gaurav.abhishek99@gmail.com>
2024-02-12 15:36:37 +0530

    Dumb way of doing input validation and authentication

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.get("/",function(req,res){
-    res.send("hey there")
+app.get("/health-checkup",function(req,res){
+    const username = req.headers.username;
+    const password = req.headers.password;
+    const kidneyId = req.query.kidneyId;
+    if(!(username == 'admin' && password=="pass")){
+        res.status(400).json({msg:"somethngs up with your input"});
+        return
+    }
+    if(kidneyId != 1 && kidneyId != 2){
+        res.status(400).json({msg:"somethngs up with your input"});
+        return
+    }
+    res.json({
+        msg: "your kidney is fine!"
+    })
 })
 
 app.listen(3000)
