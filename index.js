2ff0396a8d61d136d5cf78f6bd336055e14702bb
Abhishek Gaurav <gaurav.abhishek99@gmail.com>
2024-02-13 12:58:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,29 @@ const express = require("express");
 
 const app = express();
 
-app.get("/health-checkup",function(req,res){
-    const username = req.headers.username;
-    const password = req.headers.password;
-    const kidneyId = req.query.kidneyId;
-    if(!(username == 'admin' && password=="pass")){
-        res.status(400).json({msg:"somethngs up with your input"});
-        return
-    }
-    if(kidneyId != 1 && kidneyId != 2){
-        res.status(400).json({msg:"somethngs up with your input"});
-        return
-    }
-    res.json({
-        msg: "your kidney is fine!"
-    })
+let numberofRequests = 0;
+
+function calculateRequests(req,res,next){
+    numberofRequests++;
+    console.log(numberofRequests);
+    next()
+}
+
+app.get("/health-checkup",calculateRequests,function(req,res){
+    // const username = req.headers.username;
+    // const password = req.headers.password;
+    // const kidneyId = req.query.kidneyId;
+    // if(!(username == 'admin' && password=="pass")){
+    //     res.status(400).json({msg:"somethngs up with your input"});
+    //     return
+    // }
+    // if(kidneyId != 1 && kidneyId != 2){
+    //     res.status(400).json({msg:"somethngs up with your input"});
+    //     return
+    // }
+    // res.json({
+    //     msg: "your kidney is fine!"
+    // })
 })
 
 app.listen(3000)
